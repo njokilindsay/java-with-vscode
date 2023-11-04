8a25b518c879b4d82a48990bc4b805ab52e67b8b
njokilindsay <njurai.njoki22@students.dkut.ac.ke>
2023-11-04 16:07:05 +0300

    first commit

---
+++ helloworld.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner reader=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=reader.nextInt();
+        System.out.println("n="+n);
+    }
+    
+}
